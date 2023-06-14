8ae1c0819ec690f561f354e488e222b2df57502a
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-06-14 19:46:06 +0200

    Created: repo from GoIT start files

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      <div className={css.btn_container}>
+        {options.map(option => {
+          return (
+            <button
+              type="button"
+              onClick={() => onLeaveFeedback(option)}
+              key={option}
+              className={css.btn}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
